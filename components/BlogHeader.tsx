4fc7f9c31254e7b8de436ecc17d3ef929eccb15d
Samuel Dowds <sammydowds1993@gmail.com>
2024-02-04 11:34:28 -0600

    feat: new dropdown menu

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -1,7 +1,5 @@
-import { PortableText } from '@portabletext/react'
+import DropdownMenuButton from 'components/DropdownMenuButton'
 import Link from 'next/link'
-
-import styles from './BlogHeader.module.css'
 
 export default function BlogHeader({
   title,
@@ -15,15 +13,11 @@ export default function BlogHeader({
   switch (level) {
     case 1:
       return (
-        <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between text-pretty">
-          <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
+        <header className="mb-10 mt-4 flex items-center md:mb-12 md:flex-row justify-between text-pretty">
+          <h1 className="text-4xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
             {title}
           </h1>
-          <h4
-            className={`mt-5 text-center text-lg md:pl-8 md:text-left ${styles.portableText}`}
-          >
-            <PortableText value={description} />
-          </h4>
+            <DropdownMenuButton />
         </header>
       )
 
