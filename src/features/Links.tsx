e4d88d2051969f28c3b926d63a4dbfec223d791e
Samuel Dowds <sammydowds1993@gmail.com>
2024-08-15 11:43:10 -0500

    new project

--- src/features/Links.tsx
+++ src/features/Links.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { HiOutlineExternalLink } from 'react-icons/hi';
 
 export const Links = () => {
   return (
@@ -14,6 +13,17 @@ export const Links = () => {
         </Link>
         <p className="font-light">
           web based workout generation, optimizing muscle groups every day
+        </p>
+      </div>
+      <div>
+        <Link
+          href="https://github.com/sammydowds/react-esp32"
+          className="flex items-center underline font-bold italic text-blue-600"
+        >
+          Micro Controller &#8594; React 
+        </Link>
+        <p className="font-light">
+          Serving a react app with an ESP32 micro controller, bringing web dev closer to the metal 
         </p>
       </div>
     </div>
